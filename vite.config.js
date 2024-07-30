f329ffc91e695d92f78fcf191ed65353d0e582bf
Fawwaz Raza <muhammadfawwaz2021@gmail.com>
2024-07-30 14:38:57 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base:"/fawwazraza-portfolio/",
+   base:"/fawwazraza-portfolio/",
   plugins: [react()],
 })
