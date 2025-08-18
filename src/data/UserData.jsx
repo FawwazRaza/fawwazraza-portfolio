6dbea18618119da59171010a49a1df55ba2168fc
Fawwaz Raza <f228813@lhr.nu.edu.pk>
2025-08-18 13:06:27 +0500

    name

--- src/data/UserData.jsx
+++ src/data/UserData.jsx
@@ -1,6 +1,6 @@
 
 export const UserData = {
-  name: "Fawwaz",
+  name: "Fawwaz Raza",
   about:"I'm Fawwaz Raza, a Computer Science student at FAST NUCES Lahore, passionate about software development, web technologies, and AI. As a Dean's Honours List awardee, I strive for excellence in both academics and practical applications of technology. My experience includes an internship at Adaxiom, where I developed a dynamic E-Commerce website, and several personal projects showcasing my technical skills. These range from a website aggregator (GariGator) to a food ordering platform (Magnum Opsem), demonstrating my proficiency in various programming languages and frameworks. I'm skilled in C/C++, JavaScript, Python, React.js, and ASP.Net, among others. I'm comfortable with SQL databases and tools like VS Code and Visual Studio. Beyond coding, I have a keen interest in graphic design, adding a creative dimension to my technical abilities. Currently, I'm focusing on advanced AI, machine learning, and data science concepts. I'm eager to collaborate on innovative projects and contribute to open-source initiatives.",
   resumeUrl: "https://drive.google.com/file/d/1mHUJiRGQfWrdg2YKG90UEm-wcc2lKUYX/view?usp=sharing",
   FooterLink: "www.linkedin.com/in/fawwazraza",
